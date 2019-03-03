029a3e0ba0d1b28de5cd2c989163fa777926d2ca
曾志杰 <zengzhijie>
2019-03-03 14:26:32 +0800

    1

---
+++ src/main/java/cn/dm/DmItemConsumerApplication.java
@@ -0,0 +1,13 @@
+package cn.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmItemConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmItemConsumerApplication.class, args);
+    }
+
+}
